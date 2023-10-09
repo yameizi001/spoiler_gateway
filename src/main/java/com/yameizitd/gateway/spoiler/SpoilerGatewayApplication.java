076381ca3e35c04ce95becbb06f9a9297103dba2
yameizi <yameizi001@gmail.com>
2023-10-09 14:35:40 +0800

    Init project

---
+++ src/main/java/com/yameizitd/gateway/spoiler/SpoilerGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.yameizitd.gateway.spoiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoilerGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpoilerGatewayApplication.class, args);
+    }
+}
